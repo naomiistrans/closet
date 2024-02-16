17c0d027c19c5a8219041b9f2de19f023b5115a2
SuperSniper1018 <fourteendaystime@gmail.com>
2024-02-16 00:11:01 -0700

    Fix install.sh?

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ if [ $USER = root ];then
 	cp closet /usr/local/bin/closet
 	chmod +x /usr/local/bin/closet
 
-	mkdir /usr/local/bin/closet
+	mkdir /usr/local/bin/closet/
 	touch /usr/local/bin/closet/run
 	echo '#!/bin/sh' > /usr/local/bin/closet/run
 	chmod +x /usr/local/bin/config/run
